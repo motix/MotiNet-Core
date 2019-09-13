d4559da93ebbe8c57021e7476bb0cb29e08178ff
minh.nguyen@motiteam.com <minh.nguyen@motiteam.com>
2019-09-13 21:51:41 +0700

    Upgrade dependencies

--- src/MotiNet.Extensions.AutoMapper/MappingExpressionExtensions.cs
+++ src/MotiNet.Extensions.AutoMapper/MappingExpressionExtensions.cs
@@ -4,22 +4,6 @@ namespace MotiNet.Extensions.AutoMapper
 {
     public static class MappingExpressionExtensions
     {
-        public static IMappingExpression SwapMemberWithOrderedMember(
-            this IMappingExpression mappingExpression,
-            params string[] members)
-        {
-            foreach (var member in members)
-            {
-                mappingExpression.ForMember(member, options => options.Ignore())
-                                 .AfterMap((source, destination) =>
-                                 {
-                                     SwapMember(source, destination, member, $"Ordered{member}", null);
-                                 });
-            }
-
-            return mappingExpression;
-        }
-
         public static IMappingExpression<TSource, TDestination> SwapMemberWithOrderedMember<TSource, TDestination>(
             this IMappingExpression<TSource, TDestination> mappingExpression,
             params string[] members)
@@ -29,23 +13,14 @@ namespace MotiNet.Extensions.AutoMapper
                 mappingExpression.ForMember(member, options => options.Ignore())
                                  .AfterMap((source, destination, context) =>
                                  {
-                                     SwapMember(source, destination, member, $"Ordered{member}", context.Mapper);
+                                     var newMember = $"Ordered{member}";
+                                     var sourceValue = source.GetType().GetProperty(newMember).GetValue(source);
+                                     var destinationValue = context.Mapper.Map(sourceValue, source.GetType().GetProperty(newMember).PropertyType, destination.GetType().GetProperty(member).PropertyType);
+                                     destination.GetType().GetProperty(member).SetValue(destination, destinationValue);
                                  });
             }
 
             return mappingExpression;
-        }
-
-        private static void SwapMember(
-            object source, object destination,
-            string originalMember, string newMember,
-            IRuntimeMapper mapper)
-        {
-            var sourceValue = source.GetType().GetProperty(newMember).GetValue(source);
-            var destinationValue =
-                mapper?.Map(sourceValue, source.GetType().GetProperty(newMember).PropertyType, destination.GetType().GetProperty(originalMember).PropertyType) ??
-                Mapper.Map(sourceValue, source.GetType().GetProperty(newMember).PropertyType, destination.GetType().GetProperty(originalMember).PropertyType);
-            destination.GetType().GetProperty(originalMember).SetValue(destination, destinationValue);
         }
     }
 }
